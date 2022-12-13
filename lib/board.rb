5fa396f3e15afb7e97a40e9561ec2738bd6db2f9
mononoken <ken.maeshima@pm.me>
2022-12-12 20:39:47 -0800

    Add Square to Board#initialize

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
+require_relative './square'
 require_relative './movement'
 
 class Board
   attr_reader :squares
 
-  def initialize(squares = Array.new(8) { Array.new(8) })
+  def initialize(squares = Array.new(8) { Array.new(8, Square.new) })
     @squares = squares
   end
 
