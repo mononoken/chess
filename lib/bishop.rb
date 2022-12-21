4bc5d34cca786ad5b4bbaa846f51d4735cb674f5
mononoken <ken.maeshima@pm.me>
2022-12-21 15:33:06 -0800

    Remove differences in #path from Bishop and Rook

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -21,31 +21,38 @@ class Bishop
   end
 
   def top_right_diagonal(origin, boundaries)
-    path(origin, [1, 1], boundaries)
+    path(origin, [1, 1], boundaries, 0, nil)
   end
 
   def bot_right_diagonal(origin, boundaries)
-    path(origin, [1, -1], boundaries)
+    path(origin, [1, -1], boundaries, 0, nil)
   end
 
   def bot_left_diagonal(origin, boundaries)
-    path(origin, [-1, -1], boundaries)
+    path(origin, [-1, -1], boundaries, 0, nil)
   end
 
   def top_left_diagonal(origin, boundaries)
-    path(origin, [-1, 1], boundaries)
+    path(origin, [-1, 1], boundaries, 0, nil)
   end
 
-  def path(coord, step, boundaries)
+  def path(coord, step, boundaries, steps, step_limit)
     next_coord = coord_step(coord, step)
 
-    return [] unless within_boundaries?(next_coord, boundaries)
+    return [] unless within_boundaries?(next_coord, boundaries) && within_step_limit?(steps, step_limit)
 
-    [next_coord] + path(next_coord, step, boundaries)
+    steps += 1
+    [next_coord] + path(next_coord, step, boundaries, steps, step_limit)
   end
 
   def coord_step(coord, step)
     [coord, step].transpose.map(&:sum)
+  end
+
+  def within_step_limit?(steps, step_limit)
+    return true if step_limit.nil?
+
+    steps < step_limit
   end
 
   def within_boundaries?(coordinates, boundaries)
