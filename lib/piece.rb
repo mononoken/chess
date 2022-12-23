e92048ee722d4be847c2f6c4d582893fb2036c60
mononoken <ken.maeshima@pm.me>
2022-12-22 22:05:03 -0800

    Extract Movement from Piece

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,40 +1,66 @@
 # frozen_string_literal: true
 
-class Piece
-  def valid_destination?(origin, destination, boundaries)
-    movement(origin, boundaries).any?(destination)
+class Movement
+  attr_reader :origin, :boundaries, :step_directions, :step_limit
+
+  def initialize(origin, boundaries, step_directions, step_limit)
+    @origin = origin
+    @boundaries = boundaries
+    @step_directions = step_directions
+    @step_limit = step_limit
+  end
+
+  def valid_destination?(destination)
+    all_moves.any?(destination)
   end
 
   private
 
-  def movement(origin, boundaries)
-    paths(origin, boundaries).flatten(1)
+  def all_moves
+    paths.flatten(1)
   end
 
-  def paths(origin, boundaries)
-    step_directions.map { |step| path(origin, step, boundaries, 0, step_limit) }
+  def paths
+    step_directions.map { |step| path(origin, step) }
   end
 
-  def path(coord, step, boundaries, steps, step_limit)
+  # Path is an array of coordinates in a given step direction.
+  def path(coord, step, steps = 0)
     next_coord = coord_step(coord, step)
 
-    return [] unless within_boundaries?(next_coord, boundaries) && within_step_limit?(steps, step_limit)
+    return [] unless within_boundaries?(next_coord) && within_step_limit?(steps)
 
     steps += 1
-    [next_coord] + path(next_coord, step, boundaries, steps, step_limit)
+    [next_coord] + path(next_coord, step, steps)
   end
 
   def coord_step(coord, step)
     [coord, step].transpose.map(&:sum)
   end
 
-  def within_step_limit?(steps, step_limit)
+  def within_step_limit?(steps)
     return true if step_limit.nil?
 
     steps < step_limit
   end
 
-  def within_boundaries?(coordinates, boundaries)
-    boundaries[:files].include?(coordinates[0]) && boundaries[:ranks].include?(coordinates[1])
+  def within_boundaries?(coord)
+    boundaries[:files].include?(coord[0]) && boundaries[:ranks].include?(coord[1])
+  end
+end
+
+class Piece
+  def valid_destination?(origin, destination, boundaries)
+    Movement.new(origin, boundaries, step_directions, step_limit).valid_destination?(destination)
+  end
+
+  private
+
+  def step_directions
+    raise "#{self.class} must implement #step_directions."
+  end
+
+  def step_limit
+    raise "#{self.class} must implement #step_limit."
   end
 end
