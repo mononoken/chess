ce3d3bdce7aaa2f783d66f97b45349ebea63e14e
mononoken <ken.maeshima@pm.me>
2022-12-31 14:21:06 -0800

    Add Knight class and acceptance test

---
+++ lib/pieces/knight.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative './piece'
+
+class Knight < Piece
+  def step_directions
+    [[1, 2], [2, 1], [1, -2], [2, -1], [-1, -2], [-2, -1], [-1, 2], [-2, 1]]
+  end
+
+  def step_limit
+    1
+  end
+end
---
+++ spec/acceptance/solo_knight_api_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require_relative './../../lib/chess'
+require_relative './../../lib/pieces/knight'
+
+RSpec.describe 'Solo Knight API' do
+  it 'only accepts valid knight moves' do
+    board = Board.new
+
+    player = Player.new
+
+    game = Chess.new(board:, player:)
+
+    knight = Knight.new
+
+    origin = [2, 0]
+
+    board.populate(knight, origin)
+
+    invalid_destination = [2, 1]
+
+    expect { game.play(player, origin, invalid_destination) }
+      .to raise_error(Chess::InvalidDestinationError)
+
+    invalid_destination = [3, 1]
+
+    expect { game.play(player, origin, invalid_destination) }
+      .to raise_error(Chess::InvalidDestinationError)
+
+    valid_destination = [3, 2]
+
+    expect { game.play(player, origin, valid_destination) }
+      .not_to raise_error
+  end
+
+  it 'moves knight across board' do
+    board = Board.new
+
+    player = Player.new
+
+    game = Chess.new(board:, player:)
+
+    knight = Knight.new
+
+    board.populate(knight, [2, 2])
+
+    game.play(player, [2, 2], [3, 4])
+    game.play(player, [3, 4], [5, 5])
+    game.play(player, [5, 5], [4, 3])
+
+    last_square = board.squares[4][3]
+
+    expect(last_square.content).to eq(knight)
+  end
+end
