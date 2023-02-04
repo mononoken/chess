c563f9042453359619a6094f20e7bd355d1d0864
mononoken <ken.maeshima@pm.me>
2023-02-04 13:19:37 -0800

    Create Players#swap and #current

--- lib/players.rb
+++ lib/players.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# Track the current player color for Chess.
 class Players
+  attr_reader :current
 
+  def initialize(current = :white)
+    @current = current
+  end
+
+  def swap
+    self.current = current == :white ? :black : :white
+  end
+
+  private
+
+  attr_writer :current
 end
--- spec/unit/players_spec.rb
+++ spec/unit/players_spec.rb
@@ -5,5 +5,33 @@ require_relative './../../lib/players'
 RSpec.describe Players do
   subject(:players) { described_class.new }
 
-  describe '#'
+  describe '#current' do
+    context 'when players is initialized' do
+      it 'returns :white' do
+        expect(players.current).to eq(:white)
+      end
+    end
+
+    context 'when #swap is sent to players once after initialize' do
+      before do
+        players.swap
+      end
+
+      it 'returns :black' do
+        expect(players.current).to eq(:black)
+      end
+    end
+
+    context 'when #swap is sent to players twice after initialize' do
+      before do
+        2.times do
+          players.swap
+        end
+      end
+
+      it 'returns :white' do
+        expect(players.current).to eq(:white)
+      end
+    end
+  end
 end
